9d87ebce8127e28d7ed4856e26a6b8d520a27dea
DeekshaSharma7 <138354906+DeekshaSharma7@users.noreply.github.com>
2024-07-12 15:02:34 +0530

    deeksha

--- src/App.js
+++ src/App.js
@@ -1,79 +1,15 @@
 import React from "react";
 import "./App.css";
-import Courses from "./components/Courses.js";
+
+import Courses from "./components/Courses";
+import AboutSection from "./components/AboutSection";
 
 function App() {
   return (
     <div className="App">
-      <Courses/>
+      <Courses />
+      <AboutSection />
     </div>
-    /*
-    <section>
-      <div>
-        grid
-        <div>
-          <div>
-            <h2>
-              <span>Short-Term Certificate Courses (30-45 Days) </span>
-            </h2>
-          </div>
-        </div>
-        <div>
-          <div>
-            <fluid-columns-repeater>
-              <div>
-                <div></div>
-              </div>
-              <div>
-                grid
-                <div>
-                  Image1
-                  <wow-image>
-                    <img
-                      src="https://static.wixstatic.com/media/nsplsh_1317476a829e4e9ab1e951f86c7e3473~mv2.jpg/v1/fill/w_339,h_260,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Image%20by%20Christopher%20Gower.jpg"
-                      alt="Deeksha"
-                    />
-                  </wow-image>
-                </div>
-                <div>
-                  <p>
-                    <span>Basic Foundstion for CS</span>
-                  </p>
-                </div>
-                <div>
-                  <ul>
-                    <li>
-                      <p>
-                        <span>Programming Language- C, Python</span>
-                      </p>
-                    </li>
-
-                    <li>
-                      <p>
-                        <span>Data Structures & Algorithms</span>
-                      </p>
-                    </li>
-
-                    <li>
-                      <p>
-                        <span>Operating System</span>
-                      </p>
-                    </li>
-
-                    <li>
-                      <p>
-                        <span>The Basic foundation for the candidates.</span>
-                      </p>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div>Image2</div>
-            </fluid-columns-repeater>
-          </div>
-        </div>
-      </div>
-    </section>*/
   );
 }
 
