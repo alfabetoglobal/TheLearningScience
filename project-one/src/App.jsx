ed6d9cfdb31e953eb0a0d66ba452201c0dbc09db
Richa Chaudhary <richachaudhary.csjm@gmail.com>
2024-07-20 07:44:04 +0530

    changes

--- project-one/src/App.jsx
+++ project-one/src/App.jsx
@@ -4,8 +4,10 @@ import Sec0 from './Components/Sec0'
 // import Sec2 from './Components/Sec2'
 // import Sec3 from './Components/Sec3'
 // import Sec4 from './Components/Sec4'
-import Sec5 from './Components/Sec5'
-import Sec6 from './Components/Sec6'
+// import Sec5 from './Components/Sec5'
+// import Sec6 from './Components/Sec6'
+import Seec7 from './Components/Seec7'
+import Sec8 from './Components/Sec8'
 
 // const App=()=> {
   function App(){
@@ -17,8 +19,10 @@ import Sec6 from './Components/Sec6'
       {/* <Sec2/> */}
       {/* <Sec3/> */}
       {/* <Sec4/> */}
-      <Sec5/>
-      <Sec6/>
+      {/* <Sec5/> */}
+      {/* <Sec6/> */}
+      <Seec7/>
+      <Sec8/>
     </div>
   )
 }
--- project-one/src/Components/Sec5.jsx
+++ project-one/src/Components/Sec5.jsx
@@ -1,40 +1,40 @@
-// import React from 'react'
-import './Sec5.css'
+// // import React from 'react'
+// import './Sec5.css'
 
-const Sec5 = () => {
-  return (
-    <>
+// const Sec5 = () => {
+//   return (
+//     <>
    
-    <div className="flex-container">
+//     <div className="flex-container">
    
 
-    <div className="flex-box">
-        Box1
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, dolor.</p>
-    </div>
-    <div className="flex-box">
-        Box2
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, quibusdam.</p>
-    </div>
-    <div className="flex-box">
-        Box3
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, doloribus!</p>
-        </div>
-    <div className="flex-box">
-        Box3
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, doloribus!</p>
-    </div>
-      </div>
-      <div className="flex-container">
-    <div className="flex-box">
-        Box1
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, dolor.</p>
-    </div>
-    <div className="flex-box">
-        Box2
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, quibusdam.</p>
-    </div>
-    </div>
+//     <div className="flex-box">
+//         Box1
+//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, dolor.</p>
+//     </div>
+//     <div className="flex-box">
+//         Box2
+//         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, quibusdam.</p>
+//     </div>
+//     <div className="flex-box">
+//         Box3
+//         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, doloribus!</p>
+//         </div>
+//     <div className="flex-box">
+//         Box3
+//         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, doloribus!</p>
+//     </div>
+//       </div>
+//       <div className="flex-container">
+//     <div className="flex-box">
+//         Box1
+//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, dolor.</p>
+//     </div>
+//     <div className="flex-box">
+//         Box2
+//         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, quibusdam.</p>
+//     </div>
+//     </div>
     
  
 
@@ -43,8 +43,8 @@ const Sec5 = () => {
  
  
  
-  </>
-  )
-}
+//   </>
+//   )
+// }
 
-export default Sec5
+// export default Sec5
